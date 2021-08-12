4e6b04e5f3c675e38fb3ed2e337ec5616e3f3777
Sebastien Guillemot <sebastiengllmt@gmail.com>
2021-08-13 01:58:21 +0900

    initial commit

---
+++ es/constants.js
@@ -0,0 +1,9 @@
+export var KEY_PREFIX = 'persist:';
+export var FLUSH = 'persist/FLUSH';
+export var REHYDRATE = 'persist/REHYDRATE';
+export var RESYNC = 'persist/RESYNC';
+export var PAUSE = 'persist/PAUSE';
+export var PERSIST = 'persist/PERSIST';
+export var PURGE = 'persist/PURGE';
+export var REGISTER = 'persist/REGISTER';
+export var DEFAULT_VERSION = -1;
